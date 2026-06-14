d67f6fc1fbc63ff7e20734957a1fae6a0280fd22
Tkanh12-10-2024 <letakhanh131@gmail.com>
2026-06-14 14:55:24 +0700

    last commit

--- Lab02/DapperApi/Models/Course.cs
+++ Lab02/DapperApi/Models/Course.cs
@@ -2,7 +2,7 @@ using DapperApi.Models;
 
 public class Course {
     public int Id {get;set;}
-    public string ? CousreName {get;set;} = "" ;
+    public string ? CousreName {get;set;} = " " ;
 
 
 }
